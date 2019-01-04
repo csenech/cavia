c4603e5f4c3b7c90e0840982dba87d4c186db751
SENECHAL <csenech@gmail.com>
2019-01-04 23:09:06 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1546634192,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/droite-sheet0.png",
+		"images/gauche-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"via.txt",
+		"moment-with-locales.min.js",
+		"adapter.js"
+	]
+}
